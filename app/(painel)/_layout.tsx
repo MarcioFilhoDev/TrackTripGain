38fbe9b57fae038b04b4d3a630be53f10807fd83
MarcioFilhoDev <marciofilho.dev@hotmail.com>
2026-06-24 17:18:10 -0300

    install lucide icons and set up tab bar icons

--- app/(painel)/_layout.tsx
+++ app/(painel)/_layout.tsx
@@ -1,11 +1,52 @@
+import { colors } from "@/constants/theme";
 import { Tabs } from "expo-router";
+import { ChartNoAxesCombinedIcon, Home, User2Icon } from "lucide-react-native";
 
 export default function PainelRoutes() {
   return (
-    <Tabs initialRouteName="home">
-      <Tabs.Screen name="dashboard" />
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="profile" />
+    <Tabs
+      initialRouteName="home"
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelVisibilityMode: "unlabeled",
+        tabBarStyle: {
+          position: "absolute",
+          marginHorizontal: "5%",
+          backgroundColor: colors.primary,
+          borderTopWidth: 0,
+          borderRadius: 4,
+          marginBottom: "2%",
+        },
+        tabBarActiveBackgroundColor: "#fff",
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: "#fff",
+        tabBarIconStyle: {
+          height: 40,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="dashboard"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <ChartNoAxesCombinedIcon size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ color, size }) => <Home size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <User2Icon size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
