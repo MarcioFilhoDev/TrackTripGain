26b919e1bacf0b5e0ebcb73e7bcd571514c00fcc
MarcioFilhoDev <marciofilho.dev@hotmail.com>
2026-07-01 11:23:48 -0300

    hidden temporarily dashboard screen

--- app/(painel)/_layout.tsx
+++ app/(painel)/_layout.tsx
@@ -1,49 +1,67 @@
 import { colors } from "@/constants/theme";
 import { Tabs } from "expo-router";
-import { ChartNoAxesCombinedIcon, Home, User2Icon } from "lucide-react-native";
+import {
+  ChartNoAxesCombinedIcon,
+  Home,
+  Scroll,
+  User2Icon,
+} from "lucide-react-native";
+import { View } from "react-native";
 
 export default function PainelRoutes() {
   return (
-    <Tabs
-      initialRouteName="home"
-      screenOptions={{
-        tabBarHideOnKeyboard: true,
-        headerShown: false,
-        tabBarLabelVisibilityMode: "unlabeled",
-        tabBarStyle: {
-          backgroundColor: colors.primary,
-          borderTopWidth: 0,
-        },
-        tabBarActiveBackgroundColor: "#fff",
-        tabBarActiveTintColor: colors.primary,
-        tabBarInactiveTintColor: "#fff",
-        tabBarIconStyle: {
-          height: 40,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="dashboard"
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <ChartNoAxesCombinedIcon size={size} color={color} />
-          ),
+    <View className="flex-1 pt-10">
+      <Tabs
+        initialRouteName="home"
+        screenOptions={{
+          tabBarHideOnKeyboard: true,
+          headerShown: false,
+          tabBarLabelVisibilityMode: "unlabeled",
+          tabBarStyle: {
+            backgroundColor: colors.primary,
+            borderTopWidth: 0,
+          },
+          tabBarActiveBackgroundColor: "#fff",
+          tabBarActiveTintColor: colors.primary,
+          tabBarInactiveTintColor: "#fff",
+          tabBarIconStyle: {
+            height: 40,
+          },
         }}
-      />
-      <Tabs.Screen
-        name="home"
-        options={{
-          tabBarIcon: ({ color, size }) => <Home size={size} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <User2Icon size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="dashboard"
+          options={{
+            href: null,
+            tabBarIcon: ({ color, size }) => (
+              <ChartNoAxesCombinedIcon size={size} color={color} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="trips"
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Scroll size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="home"
+          options={{
+            tabBarIcon: ({ color, size }) => <Home size={size} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <User2Icon size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
