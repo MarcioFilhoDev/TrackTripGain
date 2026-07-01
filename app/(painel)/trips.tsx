5e9f27dcc5a37bd141a3262f61ffb00d09f7a13f
MarcioFilhoDev <marciofilho.dev@hotmail.com>
2026-07-01 14:23:44 -0300

    created filter for search trips based on customer name

--- app/(painel)/trips.tsx
+++ app/(painel)/trips.tsx
@@ -2,12 +2,14 @@ import TripItem from "@/components/TripItem";
 import { colors } from "@/constants/theme";
 import useGetTrips from "@/hooks/useGetTrips";
 import { useFocusEffect } from "expo-router";
-import { PlusCircle } from "lucide-react-native";
+import { PlusCircle, Search, X } from "lucide-react-native";
 import React, { useCallback, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
+  Pressable,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
@@ -15,6 +17,12 @@ import {
 export default function Trips() {
   const { loadTrips, tripList, loading } = useGetTrips();
   const [limit, setLimit] = useState(0);
+
+  const [search, setSearch] = useState("");
+  const filterTrips =
+    search.length > 0
+      ? tripList.filter((trip) => trip.customer.includes(search))
+      : tripList;
 
   useFocusEffect(
     useCallback(() => {
@@ -31,9 +39,26 @@ export default function Trips() {
 
   return (
     <View className="flex-1 px-[10%] pb-[5%] bg-background pt-[50]">
-      <Text className="text-text text-[26px] font-medium mb-8 ">
+      <Text className="text-text text-[26px] font-medium">
         Confira suas viagens realizadas
       </Text>
+
+      <View className="flex-row items-center gap-2 mt-4 mb-2 bg-white px-4 rounded-lg elevation">
+        <Search size={20} color={colors.placeholder} />
+        <TextInput
+          value={search}
+          onChangeText={(text) => setSearch(text)}
+          placeholder="digite algo"
+          placeholderTextColor={colors.placeholder}
+          className="flex-1"
+        />
+
+        {search.length > 0 && (
+          <Pressable onPress={() => setSearch("")}>
+            <X size={20} color={colors.placeholder} />
+          </Pressable>
+        )}
+      </View>
 
       {loading ? (
         <View className="flex-1 justify-center items-center">
@@ -42,14 +67,18 @@ export default function Trips() {
       ) : (
         <FlatList
           showsVerticalScrollIndicator={false}
-          data={tripList.slice(0, limit)}
+          data={filterTrips.slice(0, limit)}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => <TripItem {...item} />}
-          ListEmptyComponent={() => (
-            <Text>Você ainda não registrou nenhuma viagem</Text>
-          )}
+          ListEmptyComponent={() =>
+            search.length > 0 || filterTrips.length < 0 ? (
+              <Text>Ops, nenhum registro foi encontrado.</Text>
+            ) : (
+              <Text>Você ainda não registrou nenhuma viagem</Text>
+            )
+          }
           ListFooterComponent={
-            limit < tripList.length ? (
+            limit < filterTrips.length && filterTrips.length > 0 ? (
               <TouchableOpacity
                 onPress={() => setLimit((prev) => prev + 5)}
                 className="flex-row items-center bg-secondary justify-center h-12 rounded-full mt-4 gap-2"
